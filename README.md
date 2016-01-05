68620f5ccab415e0674101cb650cc8bbef49212b
Nigel Charman <nigel.charman.nz@gmail.com>
2016-01-06 07:25:27 +1300

    Created per spec serial using static instance.
    This has issues with unclosed browsers, see the README.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# Global Serial
+# Per Spec Serial
 
-* Uses the parallel Concordion runner to run the specs.
-* Creates a WebDriver instance per thread.
+* Creates a browser instance per specification.
+* The browser is created using @BeforeSpecification and removed using @AfterSpecification, and is static
+
+## Issues
+
+* For nested specs, we end up with orphaned browsers since we get @BeforeSpecification, @BeforeSpecification, @AfterSpecification, @AfterSpecification. The 2nd @BeforeSpecification annotation overwrites the browser instance created by the first, which is not cleaned up.
--- src/test/java/org/concordion/google/calculator/GoogleBaseFixture.java
+++ src/test/java/org/concordion/google/calculator/GoogleBaseFixture.java
@@ -1,12 +1,8 @@
 package org.concordion.google.calculator;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.concordion.api.AfterSuite;
+import org.concordion.api.AfterSpecification;
+import org.concordion.api.BeforeSpecification;
 import org.concordion.api.extension.Extension;
-import org.concordion.api.extension.Extensions;
-import org.concordion.ext.ParallelRunExtension;
 import org.concordion.ext.ScreenshotExtension;
 import org.concordion.google.web.Browser;
 import org.concordion.google.web.GoogleResultsPage;
@@ -20,40 +16,30 @@ import org.junit.runner.RunWith;
  * at the Google search page, and closes the browser once the test is complete.
  */
 @RunWith(ConcordionRunner.class)
-@Extensions(ParallelRunExtension.class)
 public abstract class GoogleBaseFixture {
 
-    private static List<Browser> browsers = new ArrayList<Browser>();
-	protected static ThreadLocal<Browser> browser = new ThreadLocal<Browser>();
+	protected static Browser browser;
 	
     @Extension
     public ScreenshotExtension extension = new ScreenshotExtension();
     
     protected GoogleResultsPage resultsPage;
 
+    @BeforeSpecification
     private void initialiseBrowser() { 
-        if (browser.get() == null) {
-            Browser newBrowser = new Browser();
-            browser.set(newBrowser);
-            browsers.add(newBrowser);
-        }
-        extension.setScreenshotTaker(new SeleniumScreenshotTaker(browser.get().getDriver()));
+        browser = new Browser();
+        extension.setScreenshotTaker(new SeleniumScreenshotTaker(browser.getDriver()));
     }
 
-	@AfterSuite
+	@AfterSpecification
 	public void close() {
-	    for (Browser browser : browsers) {
-	        browser.close();
-	    }
+        browser.close();
 	}
 
     /**
      * Searches for the specified topic, and waits for the results page to load.
      */
     public void searchFor(String topic) {
-        if (browser.get() == null) {
-            initialiseBrowser();
-        }
-        resultsPage = new GoogleSearchPage(browser.get().getDriver()).searchFor(topic);
+        resultsPage = new GoogleSearchPage(browser.getDriver()).searchFor(topic);
     }
 }
