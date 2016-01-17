6352f20e661a843d7fdb3b654a6c90830fc1c33f
Nigel Charman <nigel.charman.nz@gmail.com>
2016-01-17 22:40:01 +1300

    Removed unneeded code

--- src/test/java/org/concordion/google/calculator/CalculatorFixture.java
+++ src/test/java/org/concordion/google/calculator/CalculatorFixture.java
@@ -2,19 +2,7 @@ package org.concordion.google.calculator;
 
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.NoSuchElementException;
 
 @RunWith(ConcordionRunner.class)
 public class CalculatorFixture extends GoogleBaseFixture {
-
-    /**
-     * Returns the result from Google calculation.
-     */
-    public String getCalculatorResult() {
-        try {
-            return resultsPage.getCalculatorResult();
-        } catch (NoSuchElementException e) {
-            return resultsPage.getTopResultTitle();
-        }
-    }
 }
