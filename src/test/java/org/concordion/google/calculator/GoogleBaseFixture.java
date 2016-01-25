af0755492ee2b76c2bd3cba5e43b3db7b3e809e1
Nigel Charman <nigel.charman.nz@gmail.com>
2016-01-26 00:08:44 +1300

    Changed visibility of extension to private

--- src/test/java/org/concordion/google/calculator/GoogleBaseFixture.java
+++ src/test/java/org/concordion/google/calculator/GoogleBaseFixture.java
@@ -25,7 +25,7 @@ public abstract class GoogleBaseFixture {
     private SeleniumScreenshotTaker screenshotTaker;
 
     @Extension
-    public ScreenshotExtension extension = new ScreenshotExtension();
+    private ScreenshotExtension extension = new ScreenshotExtension();
 
     protected GoogleResultsPage resultsPage;
 
