98e0c42c77365f34c4b64e643dec97260a1f68f9
Nigel Charman <nigel.charman.nz@gmail.com>
2016-03-27 17:17:38 +1300

    Formatted source code

--- src/test/java/org/concordion/google/web/Browser.java
+++ src/test/java/org/concordion/google/web/Browser.java
@@ -4,19 +4,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
- * Manages the browser session. 
+ * Manages the browser session.
  */
 public class Browser {
     private WebDriver driver;
-    
+
     public Browser() {
         driver = new FirefoxDriver();
     }
-    
+
     public void close() {
         driver.close();
     }
-    
+
     public WebDriver getDriver() {
         return driver;
     }
