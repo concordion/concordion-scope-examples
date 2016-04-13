2ba792de81a4e663c654be9849349787cbee6030
Nigel Charman <nigel.charman.nz@gmail.com>
2016-04-13 20:28:07 +1200

    Added example hooks to show login/logout per example

--- src/test/java/demo/google/calculator/GoogleBaseFixture.java
+++ src/test/java/demo/google/calculator/GoogleBaseFixture.java
@@ -1,8 +1,6 @@
 package demo.google.calculator;
 
-import org.concordion.api.ConcordionScoped;
-import org.concordion.api.Scope;
-import org.concordion.api.ScopedObjectHolder;
+import org.concordion.api.*;
 import org.concordion.api.extension.Extension;
 import org.concordion.ext.ScreenshotExtension;
 import org.concordion.integration.junit4.ConcordionRunner;
@@ -16,6 +14,8 @@ import demo.driver.google.web.GoogleSearchPage;
 /**
  * A base class for Google search tests that opens up the Google site at the
  * Google search page, and closes the browser once the test is complete.
+ *
+ * This fixture shows how you could login/logout per example, with a browser created per spec.
  */
 @RunWith(ConcordionRunner.class)
 public abstract class GoogleBaseFixture {
@@ -24,6 +24,7 @@ public abstract class GoogleBaseFixture {
     private ScopedObjectHolder<Browser> browserHolder = new ScopedObjectHolder<Browser>() {
         @Override
         public Browser create() {
+            System.out.println("creating browser");
             Browser browser = new Browser();
             extension.setScreenshotTaker(new SeleniumScreenshotTaker(browser));
             return browser;
@@ -31,6 +32,7 @@ public abstract class GoogleBaseFixture {
 
         @Override
         protected void destroy(Browser browser) {
+            System.out.println("closing browser");
             browser.close();
         };
     };
@@ -40,10 +42,24 @@ public abstract class GoogleBaseFixture {
 
     protected GoogleResultsPage resultsPage;
 
+    @BeforeExample
+    public void login() {
+        Browser browser = browserHolder.get();
+        System.out.println("logging in");
+        // login would go here
+    }
+
+    @AfterExample
+    public void logout() {
+        System.out.println("logging out");
+        // logout would go here
+    }
+
     /**
      * Searches for the specified topic, and waits for the results page to load.
      */
     public void searchFor(String topic) {
+        System.out.println("searching for " + topic);
         resultsPage = new GoogleSearchPage(browserHolder.get()).searchFor(topic);
     }
 }
