4eebb9c9781385af4745c181116a7b48f69aaef3
Nigel Charman <nigel.charman.nz@gmail.com>
2017-07-02 23:11:41 +1200

    Fixed intermittent failure

--- src/test/java/demo/driver/google/web/GoogleResultsPage.java
+++ src/test/java/demo/driver/google/web/GoogleResultsPage.java
@@ -1,74 +1,70 @@
 package demo.driver.google.web;
 
-import java.util.List;
-
 import org.concordion.selenium.Browser;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * A WebDriver Page Object corresponding to the Google Results Page.
  */
 public class GoogleResultsPage {
-	
-    @CacheLookup
-	@FindBy(id = "res") 
-	private WebElement resultWrapper;
 
-    @CacheLookup
-	@FindBy(className = "g")
-	private WebElement firstResultLink;
-	
-    @CacheLookup
+    @FindBy(id = "res")
+    private WebElement resultWrapper;
+
+    @FindBy(className = "g")
+    private WebElement firstResultLink;
+
     @FindBy(id = "cwos")
     private WebElement calcResultLink;
-    
-    @CacheLookup
+
     @FindBy(css = ".vk_ans")
     private WebElement constantResultLink;
 
-    @CacheLookup
     @FindBy(css = "#_Cif > input")
     private WebElement conversionResultLink;
 
     private final WebDriver driver;
 
-	/**
-	 * Initialises the results page and waits for the page to fully load.
-	 * Assumes that the results page is already loading.
-	 */
-    public GoogleResultsPage(Browser browser) {
-		this.driver = browser.getDriver();
+    /**
+     * Initialises the results page and waits for the page to fully load.
+     * Assumes that the results page is already loading.
+     */
+    public GoogleResultsPage(Browser browser, String query) {
+        this.driver = browser.getDriver();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
         PageFactory.initElements(driver, this);
-        waitForFooter();
-	}
+        waitForTitleStartingWith(query);
+    }
 
-	/**
-	 * Checks whether the specified text occurs in any result on the results page.
-	 */
+    /**
+     * Checks whether the specified text occurs in any result on the results page.
+     */
     public boolean resultsContain(String text) {
-		List<WebElement> resultsText = resultWrapper.findElements(By.className("s"));
-		for (WebElement result : resultsText) {
+        List<WebElement> resultsText = resultWrapper.findElements(By.className("s"));
+        for (WebElement result : resultsText) {
             if (result.getText().contains(text)) {
                 return true;
             }
         }
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * Returns the text of the topmost result from the results page.
-	 */
+    /**
+     * Returns the text of the topmost result from the results page.
+     */
     public String getTopResultTitle() {
-		return firstResultLink.getText();
-	}
-	
+        return firstResultLink.getText();
+    }
+
     /**
      * Returns the text of the topmost result from the results page.
      */
@@ -76,7 +72,7 @@ public class GoogleResultsPage {
         String result = calcResultLink.getText();
         return result;
     }
-    
+
     public String getConstantResult() {
         String result = constantResultLink.getText();
         return result;
@@ -86,9 +82,13 @@ public class GoogleResultsPage {
         String result = conversionResultLink.getAttribute("value");
         return result;
     }
-    
-    private void waitForFooter() {
-		WebDriverWait wait = new  WebDriverWait(driver, 30);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("foot")));
-	}
+
+    private void waitForTitleStartingWith(String query) {
+        WebDriverWait wait = new  WebDriverWait(driver, 10);
+        wait.until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().startsWith(query);
+            }
+        });
+    }
 }
