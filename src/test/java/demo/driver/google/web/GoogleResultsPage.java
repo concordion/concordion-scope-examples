8f02e51dfae379b55abbfe50b6feb5a33543fb02
Nigel Charman <nigel.charman.nz@gmail.com>
2017-07-02 23:13:32 +1200

    Fixed intermittent failure

--- src/test/java/demo/driver/google/web/GoogleResultsPage.java
+++ src/test/java/demo/driver/google/web/GoogleResultsPage.java
@@ -4,7 +4,6 @@ import org.concordion.selenium.Browser;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedCondition;
